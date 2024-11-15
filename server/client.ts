f38591d836453679f667be308ba2ff115b3335d0
Walter Shewmake <waltershewmake@gmail.com>
2024-11-15 16:41:19 -0700

    style: add formatter

--- server/client.ts
+++
@@ -1,9 +0,0 @@
-export class Client {
-    private id: string;
-    public isHost: boolean;
-
-    constructor(id: string, isHost: boolean) {
-        this.id = id;
-        this.isHost = isHost;
-    }
-}
--- server/index.ts
+++ server/index.ts
@@ -1,97 +1,89 @@
-import { Server } from "socket.io";
-import express, { json } from "express";
 import { createServer } from "node:http";
-import { z } from "zod";
-import {
-  adjectives,
-  animals,
-  NumberDictionary,
-  uniqueNamesGenerator,
-} from "unique-names-generator";
 import cors from "cors";
+import express from "express";
+import { Server } from "socket.io";
+import {
+	NumberDictionary,
+	adjectives,
+	animals,
+	uniqueNamesGenerator,
+} from "unique-names-generator";
+import { z } from "zod";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
 });
 
 app.use(
-  cors({
-    origin: "*",
-  })
+	cors({
+		origin: "*",
+	}),
 );
 
 const numbers = NumberDictionary.generate({ min: 0, max: 999999, length: 6 });
 
 const getRoomName = () => {
-  return uniqueNamesGenerator({
-    dictionaries: [numbers, adjectives, animals],
-    separator: "-",
-  });
+	return uniqueNamesGenerator({
+		dictionaries: [numbers, adjectives, animals],
+		separator: "-",
+	});
 };
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
 });
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+	console.log("A user connected");
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+	socket.on("disconnect", () => {
+		console.log("A user disconnected");
+	});
 
-  socket.on("host", () => {
-    console.log("host");
-    const room = getRoomName();
+	socket.on("error", (data) => {
+		console.error(data);
+	});
 
-    socket.join(room);
-    socket.emit("room", room);
-  });
+	socket.on("host", () => {
+		console.log("host");
+		const room = getRoomName();
 
-  socket.on("command", (data) => {
-    console.log(`command: ${data}`);
-    const { command, room } = z
-      .object({
-        command: z.string(),
-        room: z.string(),
-      })
-      .parse(JSON.parse(data));
+		socket.join(room);
+		socket.emit("room", room);
+	});
 
-    socket.join(room);
+	socket.on("command", (data) => {
+		console.log(`command: ${data}`);
+		const { command, room } = z
+			.object({
+				command: z.string(),
+				room: z.string(),
+			})
+			.parse(data);
 
-    io.to(room).emit("command", command);
-  });
+		socket.join(room);
 
-  socket.on("error", (data) => {
-    // console.log(`response: ${data.response}`)
-    // console.log(`room: ${data.room}`)
-    // console.log(`response: ${data}`)
-    // const { response, room } = z.object({
-    //     response: z.string(),
-    //     room: z.string(),
-    // }).parse(JSON.parse(data))
+		io.to(room).emit("command", command);
+	});
 
-    io.to(data.room).emit("error", data.response);
-  });
+	socket.on("response", (data) => {
+		console.log(`response: ${data}`);
+		const { response, room } = z
+			.object({
+				response: z.string(),
+				room: z.string(),
+			})
+			.parse(data);
 
-  socket.on("response", (data) => {
-    // console.log(`response: ${data.response}`)
-    // console.log(`room: ${data.room}`)
-    // console.log(`response: ${data}`)
-    // const { response, room } = z.object({
-    //     response: z.string(),
-    //     room: z.string(),
-    // }).parse(JSON.parse(data))
-
-    io.to(data.room).emit("response", data.response);
-  });
+		io.to(room).emit("response", response);
+	});
 });
 
 server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+	console.log("Server is listening on port 3000");
 });
